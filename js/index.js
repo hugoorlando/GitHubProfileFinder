6acad35b6409bc2427eea702773e6081e3eab960
iamhugoorlando <hugo.orlando2030@gmail.com>
2021-02-05 00:42:08 -0300

    js actualizado

--- js/index.js
+++ js/index.js
@@ -1,2 +1,3 @@
 const profile_div = document.getElementById('profile-div').style.display = 'none';
 const state = document.getElementById('state').style.display = 'none';
+const section_3 = document.getElementById('section-3').style.display = 'none';
